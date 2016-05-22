a7f21914bbc7785751fb8630e1e5df1bcab302e1
Adam Deller <a.deller@ucl.ac.uk>
2016-05-22 20:02:37 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def readme():
         return f.read()
 
 setup(name='sspals',
-      version='0.0.2',
-      description='for processing single-shot positron annihlation lifetime spectra',
+      version='0.1.0',
+      description='process single-shot positron annihlation lifetime spectra',
       url='https://github.com/PositroniumSpectroscopy/sspals',
       author='Adam Deller',
       author_email='a.deller@ucl.ac.uk',
       license='BSD',
       packages=['sspals'],
       install_requires=[
-          'scipy>0.14',
+          'scipy>0.14', 'numpy>1.10', 'pandas>0.17 ',
       ],
       include_package_data=True,
       zip_safe=False)
