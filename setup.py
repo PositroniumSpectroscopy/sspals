b59e4477b576a3678eaf68162e64ffd05be5a2e7
ad3ller <a.deller@ucl.ac.uk>
2018-08-22 21:28:11 +0100

    update classifiers

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
     "Topic :: Scientific/Engineering :: Physics", 
 ]
 
